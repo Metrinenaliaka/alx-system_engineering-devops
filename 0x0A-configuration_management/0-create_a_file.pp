e0effc1ba9bc33a675f06a9efcfda184f9a90b6b
Metrinenaliaka <metrinenam@gmail.com>
2024-01-19 02:19:48 -0800

    fixed errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#creating a file in /tmp
-file{'/tmp/school':
-  ensure => 'file',
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet'
+#Create a file in `/tmp/school`
+
+file {'/tmp/school':
+ensure  => present,
+content => 'I love Puppet',
+group   => 'www-data',
+owner   => 'www-data',
+mode    => '0744'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,13 @@
-#manifest that kills a process named killmenow.
-exec{'pkill_killmenow':  
-  command => '/usr/bin/pkill killmenow'
+#kill a process called 'killmenow'
+
+# Only kill the process if it exist
+
+# If it doesn't exists, puppet should exit
+
+# with a 0 return code and not 1
+
+
+exec {'kill `killmenow` process':
+command => '/usr/bin/pkill -9 -f killmenow',
+onlyif  => '/usr/bin/pgrep -f killmenow'
 }
