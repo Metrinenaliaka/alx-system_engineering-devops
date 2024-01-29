482c8ad9bbb81221dfcfcd9dd3da00103b679eb3
Metrinenaliaka <128808519+Metrinenaliaka@users.noreply.github.com>
2024-01-29 19:29:31 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,13 @@
 # Setup New Ubuntu server with nginx
 # and add a custom HTTP header
 
-exec { 'update system':
+exec { 'update_system':
         command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
 	ensure => 'installed',
-	require => Exec['update system']
+	require => Exec['update_system']
 }
 
 file {'/var/www/html/index.html':
